2d7f04256189592b75cdbf3d3ea4896c5dfe5b59
sneeu-leeu <sa070197@gmail.com>
2022-02-09 18:01:18 +0200

    Create JS function to populate HTML placeholder

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
-let books = [new Book("book1", "Author1"),
-            new Book("book2", "Author2"),
-            new Book("book3", "Author3")];
+let books = [new Book("book1", "Author1"), new Book("book2", "Author2"), new Book("book3", "Author3")];
 
 function Book(title, author) {
   this.title = title;
   this.author = author;
 }
 
-window.addEventListener('')
+function populateBooks() {
+  let bookList = document.querySelector('.book-list');
+
+  books.forEach(book => {
+    let bookContainer = document.createElement('div');
+    let bookTitle = document.createElement('p');
+    bookTitle.innerHTML = book.title
+    let bookAuthor = document.createElement('p');
+    bookAuthor.innerHTML = book.author
+    bookContainer.append(bookTitle, bookAuthor);
+    bookList.append(bookContainer);
+  });
+  console.log(bookList);
+}
+
+window.addEventListener('load', populateBooks);
