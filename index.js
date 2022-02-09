8a7c6b7d23ee3b9823c6ea128786311a655a579a
sneeu-leeu <sa070197@gmail.com>
2022-02-09 17:49:43 +0200

    Create JS function to add books

---
+++ index.js
@@ -0,0 +1,10 @@
+let books = [new Book("book1", "Author1"),
+            new Book("book2", "Author2"),
+            new Book("book3", "Author3")];
+
+function Book(title, author) {
+  this.title = title;
+  this.author = author;
+}
+
+window.addEventListener('')
