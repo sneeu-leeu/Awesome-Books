4d26b9e6f4c02f2646e7d27b1845c6664d98fa61
sneeu-leeu <sa070197@gmail.com>
2022-02-10 21:24:02 +0200

    update add functionality

--- index.js
+++ index.js
@@ -1,34 +1,58 @@
-let books = [];
+let books = [
+  new Book('b1', 'a1'),
+  new Book('b2', 'a2'),
+  new Book('b3', 'a3'),
+  new Book('b4', 'a4')
+  ];
 
 function Book(title, author) {
-  this.title = title;
-  this.author = author;
+    this.title = title;
+    this.author = author;
 }
 
 function populateBooks() {
   let bookList = document.querySelector('.book-list');
-  bookList.innerHTML = '',
+  bookList.innerHTML = '';
 
   books.forEach(book => {
+
     let bookContainer = document.createElement('div');
     let bookTitle = document.createElement('p');
     bookTitle.innerHTML = book.title;
     let bookAuthor = document.createElement('p');
     bookAuthor.innerHTML = book.author;
-    bookContainer.append(bookTitle, bookAuthor);
+    
+    const removeButton = document.createElement('button');
+    removeButton.classList.add('remove-btn');
+    removeButton.innerHTML = 'Remove';
+    removeButton.addEventListener('click', removeBook);
+
+    bookContainer.append(bookTitle, bookAuthor, removeButton);
     bookList.append(bookContainer);
   });
-  console.log(bookList);
 }
 
-function addBook(a) {
+function addBook(e) {
   const title = document.getElementById('title').value;
   const author = document.getElementById('author').value;
-
   books.push(new Book(title, author));
-
   populateBooks();
-
-  a.preventDefault();
+  e.preventDefault();
 }
-// window.addEventListener('load', populateBooks);
+
+function removeBook(e) {
+
+  let removeButtons = document.querySelector('.removeButton');
+  let bookIndex = 0;
+
+  removeButtons.forEach((removeButton, index) => {
+    if ( e.target === removeButton ) {
+      bookIndex = index;
+    } 
+  });
+}
+
+const addButton = document.getElementById("add-btn");
+addButton.addEventListener('click', addBook);
+
+window.addEventListener('load', populateBooks);
